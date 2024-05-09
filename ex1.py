4750a4a34aef7649ae496e00b99b20a04ebf3ada
Nathalia Coutinho <106166403+NathaliaCout@users.noreply.github.com>
2024-05-09 16:18:00 -0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print("Meu primeiro código!Lest go")
+
+
+n1 = int(input('informe um valor: '))
+n2 = int(input('informe outro: '))
+
+s = n1+ n2
+
+#print(f" a soma entre {n1} e {n2} é de {s}")
+
+print(' a soma entre {} e {} é de {}'.format(n1, n2, s))
+
+print('deu certo')
