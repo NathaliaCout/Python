8ee8baeb7336972c13e60fa634779756ba29ca2f
Nathalia Coutinho <106166403+NathaliaCout@users.noreply.github.com>
2024-10-08 21:36:43 -0300

    Create ex06.py

---
+++ ex06.py
@@ -0,0 +1,9 @@
+x = (float(input('infome um numero: ')))
+
+result = x *2
+
+triplo = x *3
+
+rq = x **(1/2)
+
+print('o dobro de {} é: {} o triplo é: {} e raiz quadrada é {}'.format(x,result,triplo,rq))
