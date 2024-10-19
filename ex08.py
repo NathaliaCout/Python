dfa73d4fdac7b755ec0cb06ed5d2135c2cb06679
Nathalia Coutinho <106166403+NathaliaCout@users.noreply.github.com>
2024-10-19 19:29:01 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,6 @@
+x = (float(input('informe um valor em metros: ')))
+
+centimetros = x * 100
+mili = x * 1000
+
+print(' vc in formou {} metros e convertido em milimetros tem {} mili e covertido em centimtro tem : {}'.format(x, mili,centimetros))
