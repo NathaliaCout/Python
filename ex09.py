677009d71fe3c9c57ae9737af8c90545c1e81466
Nathalia Coutinho <106166403+NathaliaCout@users.noreply.github.com>
2024-10-24 21:48:31 -0300

    Create ex09.py

---
+++ ex09.py
@@ -0,0 +1,14 @@
+n =  (int(input("informe um numero: ")))
+
+r1 = n * 1
+r2 = n * 2
+r3 = n * 3
+r4 = n * 4
+r5 = n * 5
+r6 = n * 6
+r7 = n * 7
+r8 = n * 8
+r9 = n * 9
+r10 = n * 10
+
+print("a tabuada de {} é : \n {} = {} * 1 \n {} = {} * 2 \n {} = {} * 3 \n {} = {} * 4 \n {} = {} * 5 \n {} = {} * 6 \n {} = {} * 7 \n {} = {} * 8 \n {} = {} * 9 \n {} = {} * 10".format(n,r1,n,r2,n,r3,n,r4,n,r5,n,r6,n,r7,n,r8,n,r9,n,r10,n))
