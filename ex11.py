616149866113f8d261f3098a499babec1f677acd
NathaliaCout <coutinhona@hotmail.com>
2024-11-05 21:59:34 -0300

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,9 @@
+larg = float(input('informe a Largura da parede: '))
+
+altu = float(input("informe a Altura da parede: "))
+
+area = larg * altu
+
+Litrostinta = area / 2
+
+print("você precida de {} litros de tinta".format(Litrostinta))
